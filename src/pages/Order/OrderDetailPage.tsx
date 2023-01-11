106a694fec1d0e8eeca823fa313225db6412a34f
parkmsms <mspak1996@gmail.com>
2023-01-11 14:51:48 +0900

    주문상세 영수증부분 수정

--- src/pages/Order/OrderDetailPage.tsx
+++ src/pages/Order/OrderDetailPage.tsx
@@ -7,30 +7,34 @@ const width = Dimensions.get('window').width;
 const SECTIONS = [
     {
         title: '카드 영수증 보기',
-        content: ['cheese', 'tomato', 'apple'],
-        content2: 'gdggd'
-    },
-    {
-        title: 'menu2',
-        content: ['a', 'b', 'c'],
-        content2: 'menu2_info'
-    },
+        productName: ['닭다리 샐러드', '(토핑)계란 추가', '(토핑)야채 추가', '발사믹 소스'],
+        price: [10500, 1000, 2000, 500],
+        fullPrice: 72000,
+        couponDiscPrice: 0,
+        pointDiscPrice: 2000,
+        payAmount: 12000,
+        payMethod: '카카오페이',
+        nickNm: '응암동라모스',
+        phoneNumber: '010-7250-3229',
+        customerReq: '양좀 더주세요'
+    }
 ];
 
 const OrderDetailPage = () => {
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-
-            <View style={OrderWrapper.MainContainer}>
-                <View style={OrderWrapper.ContentsBox}>
-                    <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black' }]}>주문의 상태</Text>
-                    <TouchableOpacity><Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>샐러디 남부터미널점</Text></TouchableOpacity>
-                    <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black', marginTop: 20 }]}>주문 내역</Text>
-                    <Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>주문 일시 : 2023.01.09.. </Text>
-                    <Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>주문 번호 : LZDF!fsgsd2GDSd</Text>
-                    <Test data={SECTIONS} />
+            <ScrollView>
+                <View style={OrderWrapper.MainContainer}>
+                    <View style={OrderWrapper.ContentsBox}>
+                        <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black' }]}>주문의 상태</Text>
+                        <TouchableOpacity><Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>샐러디 남부터미널점</Text></TouchableOpacity>
+                        <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black', marginTop: 20 }]}>주문 내역</Text>
+                        <Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>주문 일시 : 2023.01.09.. </Text>
+                        <Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: '600', marginTop: 5 }]}>주문 번호 : LZDF!fsgsd2GDSd</Text>
+                        <Test data={SECTIONS} />
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 };
@@ -44,7 +48,7 @@ export const OrderWrapper = StyleSheet.create({
 
     ContentsBox: {
         borderBottomWidth: 1,
-        width: width * 0.95,
+        width: width * 0.92,
         marginTop: 20,
         sborderRadius: 1,
         paddingLeft: 12,
--- src/pages/Order/Test.tsx
+++ src/pages/Order/Test.tsx
@@ -1,50 +1,125 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React, { useState, } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
 import Accordion from 'react-native-collapsible/Accordion';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 
 const AccordionView = (props: any) => {
     const [activeSections, setActiveSections] = useState([])
+    const [open, setOpen] = useState<boolean>(false)
 
     const renderHeader = (section: any) => {
         return (
-            <View >
-                <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black' }]}>{section.title}</Text>
+            <View style={OrderWrapper.horizontal}>
+                <Text style={[OrderWrapper.FontText, { fontSize: 20, fontWeight: 'bold', color: 'black', flex: 9 }]}>{section.title}</Text>
+                {open == true ?
+                    <Icon style={{ flex: 1 }} name="ios-chevron-up-sharp" size={20} />
+                    :
+                    <Icon style={{ flex: 1 }} name="ios-chevron-down-sharp" size={20} />}
             </View>
         );
     };
 
-    useEffect(() => {
-        setActiveSections(props.data)
-    }, [])
-
     const renderContent = (section: any) => {
         return (
-            <View >
-                <Text>{section.content2}</Text>
-                {section.content?.map((menu: any, index: number) => {
-                    return (
-                        <SafeAreaView style={{ flexDirection: 'column' }} key={index}>
-                            <Text>
-                                {menu}
-                            </Text>
-                        </SafeAreaView>
-                    )
-                })}
-            </View>
+            <SafeAreaView >
+                <ScrollView>
+                    {/* 상품정보 */}
+                    <View style={OrderWrapper.rowElement}>
+                        <View style={OrderWrapper.vertical}>
+                            {section.productName?.map((item: any, index: number) => {
+                                return (
+                                    <Text style={OrderWrapper.FontText} key={index}>
+                                        {item}
+                                    </Text>
+                                )
+                            })}
+                        </View>
+                        <View style={OrderWrapper.vertical}>
+                            {section.price?.map((item: any, index: number) => {
+                                return (
+                                    <Text style={OrderWrapper.rightText} key={index}>
+                                        {item}
+                                    </Text>
+                                )
+                            })}
+                        </View>
+                    </View>
+
+                    {/* 총액 */}
+                    <View style={OrderWrapper.rowElement}>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={[OrderWrapper.FontText, { fontWeight: 'bold', color: 'black' }]}>총 상품 금액</Text>
+                        </View>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={OrderWrapper.rightText}>{section.fullPrice}</Text>
+                        </View>
+                    </View>
+
+                    {/* 할인 */}
+                    <View style={OrderWrapper.rowElement}>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={OrderWrapper.FontText}>할인(쿠폰)</Text>
+                            <Text style={OrderWrapper.FontText}>할인(포인트)</Text>
+                        </View>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={OrderWrapper.rightText}>{section.couponDiscPrice}</Text>
+                            <Text style={OrderWrapper.rightText}>{section.pointDiscPrice}</Text>
+                        </View>
+                    </View>
+
+                    {/* 결제 */}
+                    <View style={OrderWrapper.rowElement}>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={[OrderWrapper.FontText, { fontWeight: 'bold', color: 'black' }]}>결제금액</Text>
+                            <Text style={OrderWrapper.FontText}>결제방식</Text>
+                        </View>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={[OrderWrapper.rightText, { fontWeight: 'bold', color: 'black' }]}>{section.payAmount}</Text>
+                            <Text style={OrderWrapper.rightText}>{section.payMethod}</Text>
+                        </View>
+                    </View>
+
+                    {/* 주문자정보 */}
+                    <View style={OrderWrapper.columnElement}>
+                        <Text style={[OrderWrapper.FontText, { fontSize: 15, fontWeight: 'bold', color: 'black' }]}>주문자 정보</Text>
+                        <View style={OrderWrapper.horizontal}>
+                            <View style={OrderWrapper.vertical}>
+                                <Text style={OrderWrapper.FontText}>닉네임</Text>
+                                <Text style={OrderWrapper.FontText}>전화번호</Text>
+                            </View>
+                            <View style={OrderWrapper.vertical}>
+                                <Text style={OrderWrapper.rightText}>{section.nickNm}</Text>
+                                <Text style={OrderWrapper.rightText}>{section.phoneNumber}</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    {/* 요청사항 */}
+                    <View style={OrderWrapper.rowElement}>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={OrderWrapper.FontText}>요청사항</Text>
+                        </View>
+                        <View style={OrderWrapper.vertical}>
+                            <Text style={OrderWrapper.rightText}>{section.customerReq}</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         );
     };
 
     //click Event ,전환
     const updateSections = (activeSections: any) => {
         setActiveSections(activeSections);
+        setOpen(!open);
     };
 
     return (
         <Accordion
             // activeSections={[0]}
-            // sections={['Section 1', 'Section 2', 'Section 3']}
-            sections={activeSections}
+            // sections={['Section 1', 'Section 2', 'Section 1']}
+            sections={props.data}
             expandMultiple={true}
             activeSections={activeSections}
             renderHeader={renderHeader}
@@ -62,9 +137,43 @@ export const OrderWrapper = StyleSheet.create({
     FontText: {
         fontFamily: 'Apple SD Gothic Neo',
         fontStyle: 'normal',
-        letterSpacing: 0.5,
+        letterSpacing: 1,
 
-    }
+    },
+    rightText: {
+        fontFamily: 'Apple SD Gothic Neo',
+        fontStyle: 'normal',
+        letterSpacing: 1,
+        textAlign: 'right'
+    },
+    rowElement: {
+        flexDirection: 'row',
+        padding: 6,
+        borderTopWidth: 1.5,
+        borderStyle: 'solid',
+        borderTopColor: 'rgba(0, 0, 0, 0.25)',
+    },
+    columnElement: {
+        flexDirection: 'column',
+        padding: 6,
+        borderTopWidth: 1.5,
+        borderStyle: 'solid',
+        borderTopColor: 'rgba(0, 0, 0, 0.25)',
+    },
+    vertical: {
+        flex: 1,
+        flexDirection: 'column',
+        marginTop: 2
+    },
+    horizontal: {
+        flexDirection: 'row',
+        marginTop: 2
+    },
+
+
+
+
+
 });
 
 export default AccordionView;
